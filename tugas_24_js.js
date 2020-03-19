94947d1079f7a99e7bc0749d549d2f8131736baf
fahmi <pammimajid@gmail.com>
2020-03-19 16:55:29 +0800

    ok

---
+++ tugas_24_js.js
@@ -0,0 +1,6 @@
+var a = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+console.log("Sebelumnya : "+a);
+var b = a.sort();
+console.log("Ascending : "+b);
+var c = b.reverse();
+console.log("Desecending : "+c);
